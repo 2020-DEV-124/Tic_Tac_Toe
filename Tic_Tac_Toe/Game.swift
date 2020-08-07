b52b3cc8cbbccfc5c0ae1138ec1a823e0e449461
99Chahak <anishant.softit@gmail.com>
2020-08-07 22:53:39 +0530

    Game class development by following tdd.

--- Tic_Tac_Toe/Game.swift
+++ Tic_Tac_Toe/Game.swift
@@ -17,10 +17,29 @@ class Game {
     }
     
     func checkWinner() -> Player? {
-//        let firstRowCombination = [0,1,2]
-        if (gameBoard[0] != 0 && gameBoard[0] == gameBoard[1] && gameBoard[0] == gameBoard[2]){
-            return Player.currentPlayer
+        var winner : Player? = nil
+        
+        let winCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        
+        for combination in winCombinations{
+            if gameBoard[combination[0]] == Player.currentPlayer.rawValue && gameBoard[combination[0]] == gameBoard[combination[1]] && gameBoard[combination[0]] == gameBoard[combination[2]] {
+                winner = Player.currentPlayer
+            }
         }
-        return nil
+        return winner
+    }
+    
+    func reset() {
+        Player.currentPlayer = .x
+        gameBoard = [Int](repeating: 0, count: 9)
     }
 }
--- Tic_Tac_ToeTests/GameTests.swift
+++ Tic_Tac_ToeTests/GameTests.swift
@@ -42,10 +42,7 @@ class GameTests: XCTestCase {
     
     func testCheckWinnerInFirstRowForCurrentUserShouldReturn_Nil(){
         let firstRowCombination = [0,1,2]
-        for index in firstRowCombination {
-            if index == 1 { game.gameBoard[index] = 2; continue}
-            game.gameBoard[index] = 1
-        }
+        self.updateGameBoardForNilCase(combination: firstRowCombination)
         let winner = game.checkWinner()
         XCTAssertNil(winner)
     }
@@ -54,9 +51,7 @@ class GameTests: XCTestCase {
     func testCheckWinnerInFirstRowForCurrentUser_X_ShouldReturn_PlayerX(){
         let firstRowCombination = [0,1,2]
         Player.currentPlayer = .x
-        firstRowCombination.forEach { (index) in
-            game.gameBoard[index] = Player.currentPlayer.rawValue
-        }
+        self.updateGameBoardSuccessCase(combination: firstRowCombination)
         let winner = game.checkWinner()
         XCTAssertEqual(winner, Player.x, "player X didn't win in first row.")
     }
@@ -64,11 +59,170 @@ class GameTests: XCTestCase {
     func testCheckWinnerInFirstRowForCurrentUser_O_ShouldReturn_PlayerO(){
         let firstRowCombination = [0,1,2]
         Player.currentPlayer = .o
-        firstRowCombination.forEach { (index) in
-            game.gameBoard[index] = Player.currentPlayer.rawValue
-        }
+        self.updateGameBoardSuccessCase(combination: firstRowCombination)
         let winner = game.checkWinner()
         XCTAssertEqual(winner, Player.o, "player O didn't win in first row.")
     }
     
+    func testCheckWinnerInSecondRowForCurrentUserShouldReturn_Nil(){
+        let secondRowCombination = [3,4,5]
+        self.updateGameBoardForNilCase(combination: secondRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInSecondRowForCurrentUser_X_ShouldReturn_PlayerX(){
+        let secondRowCombination = [3,4,5]
+        Player.currentPlayer = .x
+        self.updateGameBoardSuccessCase(combination: secondRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.x, "player X didn't win in Second row.")
+    }
+    
+    func testCheckWinnerInSecondRowForCurrentUser_O_ShouldReturn_PlayerO(){
+        let secondRowCombination = [3,4,5]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: secondRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in Second row.")
+    }
+    
+    func testCheckWinnerInThirdRowForCurrentUserShouldReturn_Nil(){
+        let thirdRowCombination = [6,7,8]
+        self.updateGameBoardForNilCase(combination: thirdRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInThirdRowForCurrentUser_X_ShouldReturn_PlayerX(){
+        let thirdRowCombination = [6,7,8]
+        Player.currentPlayer = .x
+        self.updateGameBoardSuccessCase(combination: thirdRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.x, "player X didn't win in Third row.")
+    }
+    
+    func testCheckWinnerInThirdRowForCurrentUser_O_ShouldReturn_PlayerO(){
+        let thirdRowCombination = [6,7,8]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: thirdRowCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in Third row.")
+    }
+    
+    func testCheckWinnerInFirstColumnForCurrentUserShouldReturn_Nil(){
+        let firstColumnCombination = [0,3,6]
+        self.updateGameBoardForNilCase(combination: firstColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInFirstColumnForCurrentUser_X_ShouldReturn_PlayerX(){
+        let firstColumnCombination = [0,3,6]
+        Player.currentPlayer = .x
+        self.updateGameBoardSuccessCase(combination: firstColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.x, "player X didn't win in First column.")
+    }
+    
+    func testCheckWinnerInFirstColumnForCurrentUser_O_ShouldReturn_PlayerO(){
+        let firstColumnCombination = [0,3,6]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: firstColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in First column.")
+    }
+    
+    func testCheckWinnerInSecondColumnForCurrentUserShouldReturn_Nil(){
+        let secondColumnCombination = [1,4,7]
+        self.updateGameBoardForNilCase(combination: secondColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInSecondColumnForCurrentUserShouldReturn_PlayerX(){
+        let secondColumnCombination = [1,4,7]
+        Player.currentPlayer = .x
+        self.updateGameBoardSuccessCase(combination: secondColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.x, "player X didn't win in Second column.")
+    }
+    
+    func testCheckWinnerInThirdColumnForCurrentUserShouldReturn_Nil(){
+        let thirdColumnCombination = [2,5,8]
+        self.updateGameBoardForNilCase(combination: thirdColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInThirdColumnForCurrentUserShouldReturn_PlayerO(){
+        let thirdColumnCombination = [2,5,8]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: thirdColumnCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in Second column.")
+    }
+    
+    func testCheckWinnerInFirstDiagonalForCurrentUserShouldReturn_Nil(){
+        let firstDiagonalCombination = [0,4,8]
+        self.updateGameBoardForNilCase(combination: firstDiagonalCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInFirstDiagonalForCurrentUserShouldReturn_PlayerO(){
+        let firstDiagonalCombination = [0,4,8]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: firstDiagonalCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in Second column.")
+    }
+    
+    func testCheckWinnerInSecondDiagonalForCurrentUserShouldReturn_Nil(){
+        let secondDiagonalCombination = [2,4,6]
+        self.updateGameBoardForNilCase(combination: secondDiagonalCombination)
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    
+    func testCheckWinnerInSecondDiagonalForCurrentUserShouldReturn_PlayerO(){
+        let secondDiagonalCombination = [2,4,6]
+        Player.currentPlayer = .o
+        self.updateGameBoardSuccessCase(combination: secondDiagonalCombination)
+        let winner = game.checkWinner()
+        XCTAssertEqual(winner, Player.o, "player O didn't win in Second column.")
+    }
+    
+    func testGameDrawCondition_ShouldReturn_Nil(){
+        game.gameBoard = [1,1,2,1,2,1,2,1,1]
+        let winner = game.checkWinner()
+        XCTAssertNil(winner)
+    }
+    
+    func testReset(){
+        game.reset()
+        XCTAssertEqual(Player.currentPlayer, .x, "on reset current player does not become X.")
+        XCTAssertEqual(game.gameBoard[1], 0, "Gameboard array's all values are not 0.")
+    }
+    
+    func updateGameBoardSuccessCase(combination : [Int]){
+        combination.forEach { (index) in
+            game.gameBoard[index] = Player.currentPlayer.rawValue
+        }
+    }
+    
+    func updateGameBoardForNilCase(combination : [Int]){
+        for index in combination {
+            if index == combination[1] { game.gameBoard[index] = 1; continue}
+            game.gameBoard[index] = 2
+        }
+    }
 }
+
